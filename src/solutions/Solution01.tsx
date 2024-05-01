83842dc5b2c2056c91e9accd6d7ac4fe963175e5
josephclander <josephclander@gmail.com>
2024-05-01 15:02:58 +0100

    Complete solution 1

--- src/solutions/Solution01.tsx
+++ src/solutions/Solution01.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import styles from "./Solution01.module.css";
 
-const Solution01 = () => {
+const Solution01: React.FC = () => {
+  const [counter, setCounter] = useState(0);
+
+  function handleIncrement(): void {
+    setCounter((prev) => prev + 1);
+  }
+
+  function handleDecrement(): void {
+    setCounter((prev) => prev - 1);
+  }
+
   return (
     <div className={styles.container}>
-      <div className={styles.counter}>0</div>
+      <div className={styles.counter}>{counter}</div>
       <div className={styles.buttons__container}>
-        <button className={styles.plus}>+</button>
-        <button className={styles.minus}>-</button>
+        <button className={styles.plus} onClick={handleIncrement}>
+          +
+        </button>
+        <button className={styles.minus} onClick={handleDecrement}>
+          -
+        </button>
       </div>
     </div>
   );
