f762c054da656e7eec95dba4473d81feb91bf4e0
josephclander <josephclander@gmail.com>
2024-05-01 14:56:13 +0100

    Add sol1 styling

--- src/solutions/Solution01.tsx
+++ src/solutions/Solution01.tsx
@@ -1,5 +1,15 @@
+import styles from "./Solution01.module.css";
+
 const Solution01 = () => {
-  return <div>ADD YOUR SOLUTION HERE</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.counter}>0</div>
+      <div className={styles.buttons__container}>
+        <button className={styles.plus}>+</button>
+        <button className={styles.minus}>-</button>
+      </div>
+    </div>
+  );
 };
 
 export default Solution01;
