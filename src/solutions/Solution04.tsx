c24a68d0d5d9561e18cbda2874d6a4b073eb1a14
josephclander <josephclander@gmail.com>
2024-05-02 20:49:23 +0100

    Add Solution04

--- src/solutions/Solution04.tsx
+++ src/solutions/Solution04.tsx
@@ -1,5 +1,41 @@
+import { useRef, useState } from "react";
+
 const Solution04 = () => {
-  return <div>ADD YOUR SOLUTION HERE</div>;
+  type weatherProps = {
+    [key: string]: string;
+  };
+
+  const weather: weatherProps = {
+    London: "Sunny ☀️",
+    Manchester: "Rainy 🌧️",
+    Brighton: "Cloudy ☁️",
+  };
+
+  const selectionRef = useRef<HTMLSelectElement>(null);
+  const [forecast, setForecast] = useState<null | string>(null);
+
+  const handleSelect = () => {
+    const selection: string | undefined = selectionRef?.current?.value;
+    if (!selection || selection === "Select an option") {
+      setForecast(null);
+      return;
+    }
+    const result = weather[selection as keyof typeof weather];
+    setForecast(result);
+  };
+
+  return (
+    <div>
+      <div>{forecast}</div>
+      <br />
+      <select ref={selectionRef} onChange={handleSelect}>
+        <option>Select an option</option>
+        <option value="London">London</option>
+        <option value="Manchester">Manchester</option>
+        <option value="Brighton">Brighton</option>
+      </select>
+    </div>
+  );
 };
 
 export default Solution04;
