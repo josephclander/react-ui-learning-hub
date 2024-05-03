6ad5204eb3f3f1c5ae8a2ca7aaea4c1279514849
josephclander <josephclander@gmail.com>
2024-05-03 20:55:52 +0100

    2nd Solution01

--- src/solutions/Solution01.tsx
+++ src/solutions/Solution01.tsx
@@ -1,31 +1,25 @@
 import { useState } from "react";
-import styles from "./Solution01.module.css";
+// import styles from "./Solution01.module.css";
 
-const Solution01: React.FC = () => {
-  const [counter, setCounter] = useState(0);
+const Solution01 = () => {
+  const [count, setCount] = useState(0);
 
-  function handleIncrement(): void {
-    setCounter((prev) => prev + 1);
-  }
+  const handleAdd = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
 
-  function handleDecrement(): void {
-    setCounter((prev) => prev - 1);
-  }
+  const handleMinus = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.counter}>{counter}</div>
-      <div className={styles.buttons__container}>
-        <button
-          className={`${styles.plus} ${styles.button}`}
-          onClick={handleIncrement}
-        >
+    <div>
+      <p>{count}</p>
+      <div>
+        <button style={{ padding: "10px" }} onClick={handleAdd}>
           +
         </button>
-        <button
-          className={`${styles.minus} ${styles.button}`}
-          onClick={handleDecrement}
-        >
+        <button style={{ padding: "10px" }} onClick={handleMinus}>
           -
         </button>
       </div>
@@ -34,3 +28,35 @@ const Solution01: React.FC = () => {
 };
 
 export default Solution01;
+
+// const Solution01: React.FC = () => {
+//   const [counter, setCounter] = useState(0);
+
+//   function handleIncrement(): void {
+//     setCounter((prev) => prev + 1);
+//   }
+
+//   function handleDecrement(): void {
+//     setCounter((prev) => prev - 1);
+//   }
+
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.counter}>{counter}</div>
+//       <div className={styles.buttons__container}>
+//         <button
+//           className={`${styles.plus} ${styles.button}`}
+//           onClick={handleIncrement}
+//         >
+//           +
+//         </button>
+//         <button
+//           className={`${styles.minus} ${styles.button}`}
+//           onClick={handleDecrement}
+//         >
+//           -
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
