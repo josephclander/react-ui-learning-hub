b28e89bfa39ef7a1c83d018f03427470ae05fdb6
josephclander <josephclander@gmail.com>
2024-05-03 17:29:33 +0100

    Add Solution11

--- src/solutions/Solution11.tsx
+++ src/solutions/Solution11.tsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 const Solution11 = () => {
-  return <div>ADD YOUR SOLUTION HERE</div>;
+  const [pwd, setPwd] = useState("");
+  const [visible, toggleVisible] = useState(false);
+
+  const handleVisibility = (): void => {
+    toggleVisible((prev) => !prev);
+  };
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setPwd(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="password">Enter Password</label>
+      <br />
+      <input
+        onChange={(event) => handleInput(event)}
+        value={pwd}
+        type="password"
+        id="password"
+      />
+      <button onClick={handleVisibility}>
+        {visible ? "Hide Password" : "View Password"}
+      </button>
+      {visible && <p>{pwd}</p>}
+      <br />
+      <br />
+    </div>
+  );
 };
 
 export default Solution11;
