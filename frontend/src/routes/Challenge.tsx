229d9c934f3987250b85b1affdaa95eb38ed484f
josephclander <josephclander@gmail.com>
2024-05-09 14:48:21 +0100

    Added functionality to highlight attempted challenges

--- frontend/src/routes/Challenge.tsx
+++ frontend/src/routes/Challenge.tsx
@@ -1,26 +1,62 @@
 import { Navigate, useLoaderData } from "react-router-dom";
+import { useState } from "react";
 
 import solutionComponents from "../solutions/SolutionIndex";
 import styles from "./Challenge.module.css";
 import { ChallengeProps } from "./Root";
 
 const Challenge = () => {
-  const challenge = useLoaderData() as ChallengeProps;
-  const SolutionComponent = solutionComponents[challenge.id];
-  const attemptClass =
-    challenge?.attempts > 0 ? `${styles.done}` : `${styles.todo}`;
+  const challengeData = useLoaderData() as ChallengeProps;
+  const [challenge, setChallenge] = useState(challengeData);
+  const [error, setError] = useState<boolean>(false);
 
   if (!challenge) {
     return <Navigate to={"*"} />;
   }
 
+  const SolutionComponent = solutionComponents[challenge.id];
+
+  const attemptClass =
+    challenge?.attempts > 0 ? `${styles.done}` : `${styles.todo}`;
+
+  const shouldShowButton = challenge?.attempts !== undefined;
+
+  const handleClick = async () => {
+    try {
+      const method: string = challenge.attempts > 0 ? "decrement" : "increment";
+      const response = await fetch(
+        `http://localhost:3001/challenges/${challenge.id}/${method}`,
+        {
+          method: "PUT",
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Request failed with status ${response.status}`);
+      }
+
+      const updatedChallenge: ChallengeProps = await response.json();
+      setChallenge(updatedChallenge);
+      setError(false);
+    } catch (error) {
+      console.error("Error updating the challenge", error);
+      setError(true);
+    }
+  };
+
   return (
     <main className={styles.Challenge__container}>
       <h1 className={styles.Challenge__heading}>CHALLENGE {challenge.id}</h1>
       <h2 className={styles.Challenge__title}>{challenge.title}</h2>
-      <button className={`${styles.Challenge__button} ${attemptClass}`}>
-        {challenge?.attempts > 0 ? "DONE" : "TODO"}
-      </button>
+      {error && <div>{error}</div>}
+      {shouldShowButton && (
+        <button
+          onClick={handleClick}
+          className={`${styles.Challenge__button} ${attemptClass}`}
+        >
+          {challenge?.attempts > 0 ? "DONE" : "TODO"}
+        </button>
+      )}
       <p className={styles.Challenge__details}>{challenge.details}</p>
       <hr className={styles.Challenge__divider} />
       {SolutionComponent ? <SolutionComponent /> : <p>No solution available</p>}
