3eb788f8934fb0d6871f22ec71559999fe13597f
josephclander <josephclander@gmail.com>
2024-05-09 12:28:25 +0100

    Add conditional color for challenge button

--- frontend/src/routes/Challenge.tsx
+++ frontend/src/routes/Challenge.tsx
@@ -7,6 +7,8 @@ import { ChallengeProps } from "./Root";
 const Challenge = () => {
   const challenge = useLoaderData() as ChallengeProps;
   const SolutionComponent = solutionComponents[challenge.id];
+  const attemptClass =
+    challenge?.attempts > 0 ? `${styles.done}` : `${styles.todo}`;
 
   if (!challenge) {
     return <Navigate to={"*"} />;
@@ -16,6 +18,9 @@ const Challenge = () => {
     <main className={styles.Challenge__container}>
       <h1 className={styles.Challenge__heading}>CHALLENGE {challenge.id}</h1>
       <h2 className={styles.Challenge__title}>{challenge.title}</h2>
+      <button className={`${styles.Challenge__button} ${attemptClass}`}>
+        {challenge?.attempts > 0 ? "DONE" : "TODO"}
+      </button>
       <p className={styles.Challenge__details}>{challenge.details}</p>
       <hr className={styles.Challenge__divider} />
       {SolutionComponent ? <SolutionComponent /> : <p>No solution available</p>}
