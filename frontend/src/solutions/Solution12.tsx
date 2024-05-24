92a0efd4d97585640c2bcfee86ba5157454fa54a
josephclander <josephclander@gmail.com>
2024-05-24 15:42:40 +0100

    Add Solution12

--- frontend/src/solutions/Solution12.tsx
+++ frontend/src/solutions/Solution12.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
+const starStyle = {
+  cursor: "pointer",
+  padding: "5px",
+};
+
 const Solution12 = () => {
-  return <div>ADD YOUR SOLUTION HERE</div>;
+  const [rating, setRating] = useState(3);
+  return (
+    <div>
+      <h3>Select Rating</h3>
+      <ul style={{ display: "flex", gap: "5px", marginTop: "10px" }}>
+        {[1, 2, 3, 4, 5].map((index) => {
+          return (
+            <li style={starStyle} onClick={() => setRating(index)}>
+              {rating >= index ? "★" : "☆"}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Solution12;
