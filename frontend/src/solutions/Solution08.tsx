e2f6d1096de1bb6aa82f64c9af10ab8e167c9182
josephclander <josephclander@gmail.com>
2024-06-04 16:25:38 +0100

    Add Solution08

--- frontend/src/solutions/Solution08.tsx
+++ frontend/src/solutions/Solution08.tsx
@@ -1,5 +1,36 @@
+import { useState, CSSProperties } from "react";
+import styles from "./Solution08.module.css";
+
 const Solution08 = () => {
-  return <div>ADD YOUR SOLUTION HERE</div>;
+  const [showOne, toggleShowOne] = useState(false);
+  const handleClick = () => {
+    toggleShowOne((prev) => !prev);
+  };
+
+  const showStyle: CSSProperties = {
+    transform: "translate(0, 0)",
+  };
+
+  const hideStyle: CSSProperties = {
+    transform: "translate(0, -150px)",
+  };
+
+  const visibilityStyle = showOne ? showStyle : hideStyle;
+
+  return (
+    <div>
+      <section className={styles.section}>
+        <h2 onClick={handleClick} className={styles.heading}>
+          Drop Down One
+        </h2>
+        <ul style={visibilityStyle} className={styles.list}>
+          <li className={styles.listItem}>A</li>
+          <li className={styles.listItem}>B</li>
+          <li className={styles.listItem}>C</li>
+        </ul>
+      </section>
+    </div>
+  );
 };
 
 export default Solution08;
