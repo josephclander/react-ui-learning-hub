3c533accd91975d1a6e7a2902670009d5a12da20
josephclander <josephclander@gmail.com>
2024-07-23 21:39:49 +0100

    Change Logo

--- next-app/app/_assets/Logo.tsx
+++ next-app/app/_assets/Logo.tsx
@@ -12,50 +12,34 @@ const Icon: React.FC<SVGLogoProps> = ({ width }) => {
       strokeLinejoin="round"
       strokeMiterlimit="1.5"
       clipRule="evenodd"
-      viewBox="0 0 600 600"
+      viewBox="0 0 400 400"
     >
-      <path
-        fill="#EBEBEB"
-        stroke="#000"
-        strokeWidth="18"
-        d="M518 116.411v374.178C518 517.308 496.308 539 469.589 539H95.411C68.692 539 47 517.308 47 490.589V116.411C47 89.692 68.692 68 95.411 68h374.178C496.308 68 518 89.692 518 116.411z"
-        transform="translate(17.5 -3.5)"
-      ></path>
-      <path
-        fill="#C0E6DE"
-        stroke="#000"
-        strokeWidth="23.89"
-        d="M518 102.645v401.71C518 523.476 504.604 539 488.105 539H76.895C60.396 539 47 523.476 47 504.355v-401.71C47 83.524 60.396 68 76.895 68h411.21C504.604 68 518 83.524 518 102.645z"
-        transform="matrix(.8068 0 0 .69618 72.08 115.16)"
-      ></path>
-      <text
-        x="252"
-        y="381"
-        fontFamily="'ArialRoundedMTBold', 'Arial Rounded MT Bold', sans-serif"
-        fontSize="154.961"
-        transform="translate(-224.517 -249.026) scale(1.76818)"
-      >
-        ?
-      </text>
-      <g>
-        <circle
-          cx="121"
-          cy="111"
-          r="11"
-          transform="translate(-60 -56.545) scale(1.54545)"
-        ></circle>
-        <circle
-          cx="121"
-          cy="111"
-          r="11"
-          transform="translate(-15.24 -56.545) scale(1.54545)"
-        ></circle>
-        <circle
-          cx="121"
-          cy="111"
-          r="11"
-          transform="translate(29.52 -56.545) scale(1.54545)"
-        ></circle>
+      <g transform="translate(-471 -959)">
+        <g transform="matrix(.96386 0 0 .96386 -1206.11 894.422)">
+          <path fill="none" d="M1740 67H2155V482H1740z"></path>
+          <clipPath id="_clip1">
+            <path d="M1740 67H2155V482H1740z"></path>
+          </clipPath>
+          <g clipPath="url(#_clip1)">
+            <path
+              fill="none"
+              d="M871 979v360c0 11.04-8.962 20-20 20H491c-11.038 0-20-8.96-20-20V979c0-11.038 8.962-20 20-20h360c11.038 0 20 8.962 20 20z"
+              transform="translate(1251.34 -927.963) scale(1.0375)"
+            ></path>
+            <path
+              fill="#7B8BDE"
+              d="M169 168l20-18c13.03 11.781 32.615-4.3 18-21l18-19-18-18c-17.133 12.778-32.619-3.424-18-20l-21-17-19 16c-14.756-12.533-28.915 6.385-20 19l-19 21 19 19c18.916-11.6 33.063 5.62 20 20l20 18z"
+              transform="rotate(90 1282.65 1021.45) scale(3.19821)"
+            ></path>
+            <path
+              fill="none"
+              stroke="#000"
+              strokeWidth="3.24"
+              d="M169 168l20-18c13.03 11.781 32.615-4.3 18-21l18-19-18-18c-17.133 12.778-32.619-3.424-18-20l-21-17-19 16c-14.756-12.533-28.915 6.385-20 19l-19 21 19 19c18.916-11.6 33.063 5.62 20 20l20 18z"
+              transform="rotate(90 1282.65 1021.45) scale(3.19821)"
+            ></path>
+          </g>
+        </g>
       </g>
     </svg>
   );
