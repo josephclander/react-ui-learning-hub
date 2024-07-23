89c8669636152b5e0f10f2e11b38bb619cbed46e
josephclander <josephclander@gmail.com>
2024-07-23 21:10:16 +0100

    Add author to footer

--- next-app/app/_components/footer/Footer.tsx
+++ next-app/app/_components/footer/Footer.tsx
@@ -1,12 +1,27 @@
 // file: next-app/app/_components/footer/Footer.tsx
 import Logo from "@/app/_assets/Logo";
 import styles from "./Footer.module.css";
+import LinkedInLogo from "@/app/_assets/LinkedInLogo";
+import YouTubeLogo from "@/app/_assets/YouTubeLogo";
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <Logo width="150px" />
-      <h2>React UI Learning Hub</h2>
+      <div className={styles.siteLogo}>
+        <Logo width="150px" />
+        <h2>React UI Learning Hub</h2>
+      </div>
+      <div className={styles.name}>
+        <p>Created by Joseph Lander</p>
+        <div className={styles.socialLogos}>
+          <a href="https://www.linkedin.com/in/josephclander/">
+            <LinkedInLogo width="19px" />
+          </a>
+          <a href="https://www.youtube.com/@splat_bang">
+            <YouTubeLogo width="25px" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
