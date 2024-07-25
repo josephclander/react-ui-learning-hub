4973e15076356606c4f464d2a18b002234ffcb18
josephclander <josephclander@gmail.com>
2024-07-25 11:12:06 +0100

    Update todo explanation

--- next-app/app/components/todo-list/Explanation.tsx
+++ next-app/app/components/todo-list/Explanation.tsx
@@ -1,16 +1,19 @@
 const Explanation = () => {
   return (
     <>
+      <h3>useState vs useRef</h3>
       <p>
         To keep track of the task list, you need to use the{" "}
         <code className="code">useState</code> hook. To access the users input
         you could use state but as it is just a reference to be added to your
         task list, <code className="code">useRef</code> will be more efficient.
       </p>
+      <h3>Semantic HTML for accessibility</h3>
       <p>
         Notice that the tasks are rendered to the page inside an unordered list.
         For accessibility reasons, it&apos;s makes sense to add semantic HTML.
       </p>
+      <h3>How to add a new task</h3>
       <p>
         Adding a new task takes the input via the{" "}
         <code className="code">inputData useRef</code> and runs it through{" "}
@@ -19,6 +22,7 @@ const Explanation = () => {
         the todos state with a function and copy the previous state before
         resetting the input.
       </p>
+      <h3>Think of the inverse approach to delete</h3>
       <p>
         The <code className="code">deleteHandler</code> works by filtering all
         values that <em>are not</em> equal to the task you want to remove.
