ecc544ffba5fd584f4f54fe5b5548c7435032fd6
josephclander <josephclander@gmail.com>
2024-08-09 13:55:54 +0100

    Update footer ux

--- next-app/app/_components/footer/Footer.tsx
+++ next-app/app/_components/footer/Footer.tsx
@@ -1,24 +1,26 @@
-// file: next-app/app/_components/footer/Footer.tsx
 import Logo from "@/app/_assets/Logo";
 import styles from "./Footer.module.css";
 import LinkedInLogo from "@/app/_assets/LinkedInLogo";
 import YouTubeLogo from "@/app/_assets/YouTubeLogo";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
       <div className={styles.siteLogo}>
         <Logo width="150px" />
-        <h2>React UI Learning Hub</h2>
+        <h2 className={styles.Home__Link}>
+          <Link href="/">React UI Learning Hub</Link>
+        </h2>
       </div>
       <div className={styles.name}>
         <p>Created by Joseph Lander</p>
         <div className={styles.socialLogos}>
           <a href="https://www.linkedin.com/in/josephclander/">
-            <LinkedInLogo width="19px" />
+            <LinkedInLogo width="25px" />
           </a>
           <a href="https://www.youtube.com/@splat_bang">
-            <YouTubeLogo width="25px" />
+            <YouTubeLogo width="35px" />
           </a>
         </div>
       </div>
