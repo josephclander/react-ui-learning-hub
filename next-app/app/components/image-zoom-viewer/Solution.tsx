d355ac4ede63ccf23a2e0bcb924d2b976361c832
josephclander <josephclander@gmail.com>
2024-08-09 13:44:14 +0100

    Fix Image Zoom Viewer image props

--- next-app/app/components/image-zoom-viewer/Solution.tsx
+++ next-app/app/components/image-zoom-viewer/Solution.tsx
@@ -36,6 +36,7 @@ const Solution = () => {
       <Image
         style={{ transformOrigin: `${position.x}px ${position.y}px` }}
         className={`${styles.image}`}
+        fill={true}
         src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="laptop on desk"
       />
--- next-app/app/components/image-zoom-viewer/solutionCss.ts
+++ next-app/app/components/image-zoom-viewer/solutionCss.ts
@@ -1,1 +1,18 @@
-export const solutionCss = `CSS HERE`;
+export const solutionCss = `.container {
+  position: relative;
+  overflow: hidden;
+  width: 600px;
+  height: 400px;
+  border: 2px solid black;
+}
+
+.image {
+  position: absolute;
+  scale: 1;
+  transition: 0.5s;
+}
+
+.container:hover .image {
+  scale: 2.5;
+}
+`;
