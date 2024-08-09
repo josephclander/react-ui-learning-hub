4e4e525cd4d190edded3c5b1279f8ffbad450546
josephclander <josephclander@gmail.com>
2024-08-09 21:13:16 +0100

    Add tooltips notes

--- next-app/app/components/tooltips-on-hover/solutionCode.ts
+++ next-app/app/components/tooltips-on-hover/solutionCode.ts
@@ -1,1 +1,22 @@
-export const solutionCode = `SOLUTION CODE HERE`;
+export const solutionCode = `import styles from "./Solution.module.css";
+
+const Solution = () => {
+  return (
+    <div>
+      <ul>
+        <li className={styles.listItem}>
+          1<span className={styles.tooltip}>Item 1</span>
+        </li>
+        <li className={styles.listItem}>
+          2<span className={styles.tooltip}>Item 2</span>
+        </li>
+        <li className={styles.listItem}>
+          3<span className={styles.tooltip}>Item 3</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Solution;
+`;
--- next-app/app/components/tooltips-on-hover/solutionCss.ts
+++ next-app/app/components/tooltips-on-hover/solutionCss.ts
@@ -1,1 +1,25 @@
-export const solutionCss = `CSS HERE`;
+export const solutionCss = `.listItem {
+  position: relative;
+  padding: 5px;
+  border: 1px solid black;
+  width: 50px;
+  text-align: center;
+  margin: 10px;
+}
+
+.tooltip {
+  position: absolute;
+  right: -130%;
+  top: 50%;
+  transform: translateY(-50%);
+  color: white;
+  background-color: grey;
+  padding: 5px;
+  border-radius: 5px;
+  visibility: hidden;
+}
+
+.listItem:hover .tooltip {
+  visibility: visible;
+}
+`;
