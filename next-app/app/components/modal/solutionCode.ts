dcc8fcc68429d2990bded3d040905bc707e17f1e
josephclander <josephclander@gmail.com>
2024-08-09 15:42:30 +0100

    Add modal code and css

--- next-app/app/components/modal/solutionCode.ts
+++ next-app/app/components/modal/solutionCode.ts
@@ -1,1 +1,52 @@
-export const solutionCode = `SOLUTION CODE HERE`;
+export const solutionCode = `import { useState } from "react";
+import styles from "./Solution.module.css";
+
+const Solution = () => {
+  const [showModal, setShowModal] = useState(false);
+  const turnOnModal = () => setShowModal(true);
+  const turnOffModal = () => setShowModal(false);
+
+  const handleContainerClick: React.MouseEventHandler<HTMLDivElement> = (
+    event
+  ) => {
+    // Check if the click is on the container itself to close the modal
+    if ((event.target as HTMLDivElement).id === "container") {
+      turnOffModal();
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      {showModal && (
+        <div
+          id="container"
+          onClick={handleContainerClick}
+          className={styles.modalContainer}
+          role="dialog"
+          aria-modal="true"
+        >
+          <div className={styles.modal}>
+            <button
+              onClick={turnOffModal}
+              aria-label="Click to close"
+              className={styles.close}
+            >
+              X
+            </button>
+            <h3>This is a modal</h3>
+          </div>
+        </div>
+      )}
+      <button
+        onClick={turnOnModal}
+        aria-label="Show Modal"
+        className={styles.open}
+      >
+        Show Modal
+      </button>
+    </div>
+  );
+};
+
+export default Solution;
+`;
--- next-app/app/components/modal/solutionCss.ts
+++ next-app/app/components/modal/solutionCss.ts
@@ -1,1 +1,43 @@
-export const solutionCss = `CSS HERE`;
+export const solutionCss = `.container {
+  position: relative;
+  background-color: #bfe6de;
+  width: 100%;
+  height: 500px;
+  display: grid;
+  place-content: center;
+}
+
+.open {
+  padding: 5px;
+  cursor: pointer;
+}
+
+.close {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 30px;
+  padding: 5px;
+  cursor: pointer;
+}
+
+.modalContainer {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  display: grid;
+  place-content: center;
+}
+
+.modal {
+  position: relative;
+  background-color: rgb(224, 224, 224);
+  width: 200px;
+  height: 200px;
+  display: grid;
+  place-content: center;
+}
+`;
