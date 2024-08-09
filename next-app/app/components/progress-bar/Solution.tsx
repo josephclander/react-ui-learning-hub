7edbc4a8ece383eafa7a8af6f2e77a0838b3ebda
josephclander <josephclander@gmail.com>
2024-08-09 19:46:53 +0100

    Add progress bar code and css notes

--- next-app/app/components/progress-bar/Solution.tsx
+++ next-app/app/components/progress-bar/Solution.tsx
@@ -1,37 +1,15 @@
 "use client";
 
 import { useState, CSSProperties } from "react";
+import styles from "./Solution.module.css";
 
 const Solution = () => {
   const [count, setCount] = useState(0);
   const [isComplete, setIsComplete] = useState(false);
-  const progressBarWidth = 400;
-
-  const inputStyle: CSSProperties = {
-    padding: "5px",
-    margin: "50px 0 20px",
-  };
-
-  const progressContainerStyle: CSSProperties = {
-    position: "relative",
-  };
 
   const progressStyle: CSSProperties = {
-    position: "absolute",
-    marginTop: "10px",
-    height: "20px",
-    // 400 / 50 = 8 -> calc proportion of length
-    width: `${Math.min(progressBarWidth, 8 * count)}px`,
-    backgroundColor: "#BFE6DE",
-    border: "1px solid black",
-  };
-
-  const progressBGStyle: CSSProperties = {
-    position: "absolute",
-    marginTop: "10px",
-    height: "20px",
-    width: `${progressBarWidth}px`,
-    border: "1px solid black",
+    // 400px / 50 characters = 8 -> calc proportion of length
+    width: `${8 * count}px`,
   };
 
   const handleCount: React.ChangeEventHandler<HTMLTextAreaElement> = (
@@ -45,12 +23,17 @@ const Solution = () => {
   return (
     <div>
       <h3>Enter a string with at least 50 characters</h3>
-      <div style={progressContainerStyle}>
-        <div style={progressBGStyle}></div>
-        <div style={progressStyle}></div>
+      <div className={styles.progressContainer}>
+        <div className={styles.progressBG}></div>
+        <div style={progressStyle} className={styles.progress}></div>
       </div>
-      <textarea onChange={handleCount} style={inputStyle} cols={46} rows={3} />
-      {isComplete && <h3 style={{ color: "red" }}>Complete!</h3>}
+      <textarea
+        className={styles.input}
+        onChange={handleCount}
+        cols={46}
+        rows={3}
+      />
+      {isComplete && <h3 className={styles.completeMsg}>Complete!</h3>}
     </div>
   );
 };
--- next-app/app/components/progress-bar/solutionCode.ts
+++ next-app/app/components/progress-bar/solutionCode.ts
@@ -1,1 +1,39 @@
-export const solutionCode = `SOLUTION CODE HERE`;
+export const solutionCode = `import { useState, CSSProperties } from "react";
+import styles from "./Solution.module.css";
+
+const Solution = () => {
+  const [count, setCount] = useState(0);
+  const [isComplete, setIsComplete] = useState(false);
+
+  const progressStyle: CSSProperties = {
+    // 400px / 50 characters = 8 -> calc proportion of length
+    width: \`$\{8 * count}px\`,
+  };
+
+  const handleCount: React.ChangeEventHandler<HTMLTextAreaElement> = (
+    event
+  ) => {
+    const newCount = event.target.value.length;
+    setCount(newCount);
+    setIsComplete(newCount >= 50);
+  };
+
+  return (
+    <div>
+      <h3>Enter a string with at least 50 characters</h3>
+      <div className={styles.progressContainer}>
+        <div className={styles.progressBG}></div>
+        <div style={progressStyle} className={styles.progress}></div>
+      </div>
+      <textarea
+        className={styles.input}
+        onChange={handleCount}
+        cols={46}
+        rows={3}
+      />
+      {isComplete && <h3 className={styles.completeMsg}>Complete!</h3>}
+    </div>
+  );
+};
+
+export default Solution;`;
