782b0ebf1fcdb7d0c029b6d7a283ce9433b4965a
josephclander <josephclander@gmail.com>
2024-08-09 21:09:36 +0100

    Add tab selector notes

--- next-app/app/components/tab-selector/solutionCode.ts
+++ next-app/app/components/tab-selector/solutionCode.ts
@@ -1,1 +1,53 @@
-export const solutionCode = `SOLUTION CODE HERE`;
+export const solutionCode = `import { useState } from "react";
+import styles from "./Solution.module.css";
+
+const Solution = () => {
+  type tabProps = "One" | "Two" | "Three";
+
+  const [activeTab, setActiveTab] = useState<tabProps>("One");
+
+  const clickHandler = (tab: tabProps) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div>
+      <ul className={styles.list}>
+        <li
+          onClick={() => clickHandler("One")}
+          className={\`\${styles.listItem} $\{
+            activeTab === "One" ? styles.active : ""
+          }\`}
+        >
+          One
+        </li>
+        <li
+          onClick={() => clickHandler("Two")}
+          className={\`$\{styles.listItem} $\{
+            activeTab === "Two" ? styles.active : ""
+          }\`}
+        >
+          Two
+        </li>
+        <li
+          onClick={() => clickHandler("Three")}
+          className={\`$\{styles.listItem} $\{
+            activeTab === "Three" ? styles.active : ""
+          }\`}
+        >
+          Three
+        </li>
+      </ul>
+      <section className={styles.content}>{content[activeTab]}</section>
+    </div>
+  );
+};
+
+export default Solution;
+
+const content: { [key: string]: string } = {
+  One: "This is the first section.",
+  Two: "This is the second section.",
+  Three: "This is the third section.",
+};
+`;
--- next-app/app/components/tab-selector/solutionCss.ts
+++ next-app/app/components/tab-selector/solutionCss.ts
@@ -1,1 +1,28 @@
-export const solutionCss = `CSS HERE`;
+export const solutionCss = `.list {
+  display: flex;
+  gap: 5px;
+}
+
+.listItem {
+  padding: 5px;
+  border: 1px solid grey;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.listItem:hover,
+.listItem:active {
+  background-color: rgb(212, 179, 135);
+  color: black;
+}
+
+.active {
+  background-color: rgb(71, 169, 114);
+  color: white;
+}
+
+.content {
+    margin-top: 20px;
+}
+`;
