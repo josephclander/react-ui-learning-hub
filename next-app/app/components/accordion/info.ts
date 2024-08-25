7a65fcaf08327dacf1efe7c9f00de11e6ef85c1d
Joseph Lander <josephclander@gmail.com>
2024-08-25 16:19:45 +0100

    Add links to accordian

--- next-app/app/components/accordion/info.ts
+++ next-app/app/components/accordion/info.ts
@@ -4,4 +4,22 @@ export const info: InfoProps = {
   title: "Accordion",
   details:
     "Build an accordion/dropdown where sections expand or collapse when their headers are clicked.",
+  links: [
+    {
+      name: "MDN Transform",
+      url: "https://developer.mozilla.org/en-US/docs/Web/CSS/transform",
+    },
+    {
+      name: "MDN Overflow",
+      url: "https://developer.mozilla.org/en-US/docs/Web/CSS/overflow",
+    },
+    {
+      name: "MDN Ternary Operator",
+      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator",
+    },
+    {
+      name: "W3 Schools Semantic HTML",
+      url: "https://www.w3schools.com/html/html5_semantic_elements.asp",
+    },
+  ],
 };
