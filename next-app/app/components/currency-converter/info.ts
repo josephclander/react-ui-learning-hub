4c62e6cc2dd02af4e9ffa9451b448cc60ae2d561
Joseph Lander <josephclander@gmail.com>
2024-08-25 16:41:43 +0100

    Add Currency converter links

--- next-app/app/components/currency-converter/info.ts
+++ next-app/app/components/currency-converter/info.ts
@@ -4,4 +4,22 @@ export const info: InfoProps = {
   title: "Currency Converter",
   details:
     "Build a small interface to convert amounts from one currency to another using a static exchange rate.",
+  links: [
+    {
+      name: "MDN Event preventDefault",
+      url: "https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault",
+    },
+    {
+      name: "MDN optionalChaining",
+      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining",
+    },
+    {
+      name: "MDN parseInt",
+      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt",
+    },
+    {
+      name: "MDN Form",
+      url: "https://developer.mozilla.org/en-US/docs/Web/HTML/Element/form",
+    },
+  ],
 };
