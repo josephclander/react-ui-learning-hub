16313b03a4fb54ffddeb78f1e6bd3dff83791e6a
Joseph Lander <josephclander@gmail.com>
2024-08-25 16:27:50 +0100

    Add Counter links

--- next-app/app/components/counter/info.ts
+++ next-app/app/components/counter/info.ts
@@ -4,4 +4,10 @@ export const info: InfoProps = {
   title: "Counter",
   details:
     "Create a component that shows a number (starts from 0). Add two buttons to increment and decrement the counter.",
+  links: [
+    {
+      name: "React Functional State Updates",
+      url: "https://legacy.reactjs.org/docs/hooks-reference.html#functional-updates",
+    },
+  ],
 };
