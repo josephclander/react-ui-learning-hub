118ca63d3f8f8f69a7882315b9a733ff3fb1f746
Joseph Lander <josephclander@gmail.com>
2024-08-25 16:37:28 +0100

    Add Counter links

--- next-app/app/components/countdown-timer/info.ts
+++ next-app/app/components/countdown-timer/info.ts
@@ -4,4 +4,26 @@ export const info: InfoProps = {
   title: "Countdown Timer",
   details:
     "Create a countdown timer where users can set a duration, and it counts down to zero with updates displayed visually.",
+  links: [
+    {
+      name: "MDN setInterval",
+      url: "https://developer.mozilla.org/en-US/docs/Web/API/setInterval",
+    },
+    {
+      name: "MDN clearInterval",
+      url: "https://developer.mozilla.org/en-US/docs/Web/API/clearInterval",
+    },
+    {
+      name: "MDN optionalChaining",
+      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining",
+    },
+    {
+      name: "React Functional State Updates",
+      url: "https://legacy.reactjs.org/docs/hooks-reference.html#functional-updates",
+    },
+    {
+      name: "React useEffect Dependency Array",
+      url: "https://react.dev/reference/react/useEffect#examples-dependencies",
+    },
+  ],
 };
