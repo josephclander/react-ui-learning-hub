617f151a05a3bcc870bacb06890a88f54ba6f9ac
Joseph Lander <josephclander@gmail.com>
2024-08-25 20:16:42 +0100

    Add pagination links

--- next-app/app/components/pagination/info.ts
+++ next-app/app/components/pagination/info.ts
@@ -4,4 +4,18 @@ export const info: InfoProps = {
   title: "Pagination",
   details:
     "Build a component that shows numbered pages and allows users to click through pages of data (data can be static).",
+  links: [
+    {
+      name: "MDN Slice",
+      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice",
+    },
+    {
+      name: "MDN Ternary Operator",
+      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator",
+    },
+    {
+      name: "React - passing arguments to event handlers",
+      url: "https://legacy.reactjs.org/docs/handling-events.html#passing-arguments-to-event-handlers",
+    },
+  ],
 };
