5b68f821adefe6ec042c40445f889793ba85a4a0
Joseph Lander <josephclander@gmail.com>
2024-08-25 18:15:02 +0100

    Add image zoom links

--- next-app/app/components/image-zoom-viewer/info.ts
+++ next-app/app/components/image-zoom-viewer/info.ts
@@ -4,4 +4,26 @@ export const info: InfoProps = {
   title: "Image Zoom Viewer",
   details:
     "Create a component that allows users to hover over an image to view a magnified version of the part their cursor is over.",
+  links: [
+    {
+      name: "Next Image optimization",
+      url: "https://nextjs.org/docs/pages/building-your-application/optimizing/images",
+    },
+    {
+      name: "MDN getBoundingClientRect",
+      url: "https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect",
+    },
+    {
+      name: "MDN event.clientX",
+      url: "https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/clientX",
+    },
+    {
+      name: "MDN transform-origin",
+      url: "https://developer.mozilla.org/en-US/docs/Web/CSS/transform-origin",
+    },
+    {
+      name: "MDN object-fit",
+      url: "https://developer.mozilla.org/en-US/docs/Web/CSS/object-fit",
+    },
+  ],
 };
