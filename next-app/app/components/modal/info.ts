e6f75793b8a691d069b67753ad136b20edf86f05
Joseph Lander <josephclander@gmail.com>
2024-08-25 18:19:22 +0100

    Add modal links

--- next-app/app/components/modal/info.ts
+++ next-app/app/components/modal/info.ts
@@ -4,4 +4,14 @@ export const info: InfoProps = {
   title: "Modal",
   details:
     "Implement a modal window that opens with a button click and can be closed with a close button or by clicking outside the modal.",
+  links: [
+    {
+      name: "MDN Event bubbling",
+      url: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Event_bubbling",
+    },
+    {
+      name: "MDN Short circuit evaluation",
+      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_AND",
+    },
+  ],
 };
