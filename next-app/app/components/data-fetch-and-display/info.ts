04529eb345923b2c3524a6bfb29be45b3140550e
Joseph Lander <josephclander@gmail.com>
2024-08-25 17:49:53 +0100

    Add data fetch links

--- next-app/app/components/data-fetch-and-display/info.ts
+++ next-app/app/components/data-fetch-and-display/info.ts
@@ -4,4 +4,26 @@ export const info: InfoProps = {
   title: "Data Fetch and Display",
   details:
     "Make a component that fetches data from a public API and displays it in a list or table format.",
+  links: [
+    {
+      name: "MDN Fetch",
+      url: "https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API",
+    },
+    {
+      name: "MDN Short circuit evaluation",
+      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_AND",
+    },
+    {
+      name: "W3 Schools Semantic HTML",
+      url: "https://www.w3schools.com/html/html5_semantic_elements.asp",
+    },
+    {
+      name: "React Custom hooks",
+      url: "https://react.dev/learn/reusing-logic-with-custom-hooks",
+    },
+    {
+      name: "MDN optionalChaining",
+      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining",
+    },
+  ],
 };
