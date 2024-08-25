138ac7f6d7a99b511ac9d05389e4dd72ec96200e
Joseph Lander <josephclander@gmail.com>
2024-08-25 17:51:33 +0100

    Add drag and drop api

--- next-app/app/components/drag-and-drop-interface/info.ts
+++ next-app/app/components/drag-and-drop-interface/info.ts
@@ -4,4 +4,14 @@ export const info: InfoProps = {
   title: "Drag and Drop Interface",
   details:
     "Implement a basic drag and drop interface where users can move items between different containers.",
+  links: [
+    {
+      name: "MDN Drag and drop API",
+      url: "https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API",
+    },
+    {
+      name: "MDN Drag and drop API",
+      url: "https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API",
+    },
+  ],
 };
