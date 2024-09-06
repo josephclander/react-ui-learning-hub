8af83f2e5be493f1e910bdacc4cea5dd9193c4e9
Joseph Lander <josephclander@gmail.com>
2024-09-06 14:00:38 +0100

    Update info links for bell and autocomplete

--- next-app/app/components/autocomplete-text-input/info.ts
+++ next-app/app/components/autocomplete-text-input/info.ts
@@ -4,4 +4,22 @@ export const info: InfoProps = {
   title: "Autocomplete Text Input",
   details:
     "Develop a text input that suggests possible completions as the user types, pulling from a predefined list of options.",
+  links: [
+    {
+      name: "CSS Tricks - Position",
+      url: "https://css-tricks.com/almanac/properties/p/position/",
+    },
+    {
+      name: "MDN Array Slice",
+      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice",
+    },
+    {
+      name: "MDN Array Filter",
+      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter",
+    },
+    {
+      name: "MDN Key Down Event",
+      url: "https://developer.mozilla.org/en-US/docs/Web/API/Element/keydown_event",
+    },
+  ],
 };
--- next-app/app/components/notification-bell/info.ts
+++ next-app/app/components/notification-bell/info.ts
@@ -4,4 +4,10 @@ export const info: InfoProps = {
   title: "Notification Bell",
   details:
     "Implement a bell icon that shows a red notification badge; clicking on the bell shows a dropdown with messages.",
+  links: [
+    {
+      name: "CSS Tricks - Position",
+      url: "https://css-tricks.com/almanac/properties/p/position/",
+    },
+  ],
 };
